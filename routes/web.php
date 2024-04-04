846492aa518d1ba9a77dd3377ac9f8eaa4b34536
vondominic <vondominic.belmonte@usant.edu.ph>
2024-04-04 12:50:00 +0800

    Add posts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/von', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts/{post}', function ($post) {
+    return view('posts.show',['post' => $post]
+);
 });
